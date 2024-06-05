4d0063e70ed5aac0cc26bf98e50062fbb429d5fd
Basirov Ilgiz <Basirov_ilgiz@mail.ru>
2024-06-06 03:02:51 +0500

    Add some code

---
+++ ParseJsonToXml.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.Json;
+using System.Xml.Linq;
+
+namespace ConvertJSONtoXML
+{
+    public class ParseJsonToXml(string json)
+    {
+        public XDocument ParseJson()
+        {
+            JsonDocument doc = JsonDocument.Parse(json);
+            XDocument xDocument = new XDocument();
+            JsonElement root = doc.RootElement;
+            XElement rootElement = new XElement("root");
+            parseElement(root, rootElement);
+            xDocument.Add(rootElement);
+            return xDocument;
+        }
+
+        private void parseElement(JsonElement root, XElement rootElement = null)
+        {
+            switch (root.ValueKind)
+            {
+                case JsonValueKind.Undefined:
+                    break;
+                case JsonValueKind.Object:
+                    parseObject(root, rootElement); break;
+                case JsonValueKind.String:
+                    parseString(root, rootElement); break;
+                case JsonValueKind.Number:
+                    parseNumber(root, rootElement); break;
+                case JsonValueKind.Array:
+                    parseArray(root, rootElement); break;
+                case JsonValueKind.True:
+                case JsonValueKind.False:
+                    parseBoolean(root, rootElement); break;
+                case JsonValueKind.Null:
+                    parseNull(); break;
+                default: throw new NotSupportedException($"Неподдерживаемый тип JSON {root.ValueKind}");
+            }
+        }
+
+        private void parseNull() => Console.WriteLine("Нулевое значение");
+        private void parseBoolean(JsonElement root, XElement rootElement) => rootElement.Value = root.GetRawText();
+        private void parseNumber(JsonElement root, XElement rootElement) => rootElement.Value = root.GetRawText();
+        private void parseString(JsonElement root, XElement rootElement) => rootElement.Value = root.GetString();
+        private void parseObject(JsonElement root, XElement rootElement)
+        {
+            foreach (var element in root.EnumerateObject())
+            {
+                XElement objectElement = new XElement("text");
+                parseElement(element.Value, objectElement);
+                rootElement.Add(objectElement);
+            }
+        }
+        private void parseArray(JsonElement root, XElement rootElement)
+        {
+            foreach (var element in root.EnumerateArray())
+            {
+                XElement objectElement = new XElement("text");
+                parseElement(root, objectElement);
+                rootElement.Add(objectElement);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿namespace ConvertJSONtoXML
+﻿using System.Xml.Linq;
+
+namespace ConvertJSONtoXML
 {
     internal class Program
     {
+        public static string json = """
+                                    {"Current":{"Time":"2023-06-18T20:35:06.722127+04:00","Temperature":29,"Weathercode":1,"Windspeed":2.1,"Winddirection":1},
+                                    "History":[{"Time":"2023-06-17T20:35:06.77707+04:00","Temperature":29,"Weathercode":2,"Windspeed":2.4,"Winddirection":1},
+                                    {"Time":"2023-06-16T20:35:06.777081+04:00","Temperature":22,"Weathercode":2,"Windspeed":2.4,"Winddirection":1},
+                                    {"Time":"2023-06-15T20:35:06.777082+04:00","Temperature":21,"Weathercode":4,"Windspeed":2.2,"Winddirection":1}]}
+                                    """;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ParseJsonToXml parseJsonToXml = new ParseJsonToXml(json);
+            XDocument xDocument = parseJsonToXml.ParseJson();
+            Console.WriteLine(xDocument);
         }
     }
 }
