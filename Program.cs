422e9d4ead0c6d4bc49023647da791e6f4710277
Basirov Ilgiz <Basirov_ilgiz@mail.ru>
2024-06-06 20:31:52 +0500

    Updated json

--- Program.cs
+++ Program.cs
@@ -4,12 +4,9 @@ namespace ConvertJSONtoXML
 {
     internal class Program
     {
-        public static string json = """
-                                    {"Current":{"Time":"2023-06-18T20:35:06.722127+04:00","Temperature":29,"Weathercode":1,"Windspeed":2.1,"Winddirection":1},
-                                    "History":[{"Time":"2023-06-17T20:35:06.77707+04:00","Temperature":29,"Weathercode":2,"Windspeed":2.4,"Winddirection":1},
-                                    {"Time":"2023-06-16T20:35:06.777081+04:00","Temperature":22,"Weathercode":2,"Windspeed":2.4,"Winddirection":1},
-                                    {"Time":"2023-06-15T20:35:06.777082+04:00","Temperature":21,"Weathercode":4,"Windspeed":2.2,"Winddirection":1}]}
-                                    """;
+        public static string json = "{\"VDD\": 44,\"REAL\": 0,\"Perem\": " +
+            "1,\"ID\": \"_6QI0HB878014\",\"ND\": \"31426\",\"DD\": " +
+            "\"2023-10-16T00:00:00\",\"ORGANIZ\": 1,\"ORG_1C\": \"000002575\",\"CM_1C\": \"00000057345\"}";
         static void Main(string[] args)
         {
             ParseJsonToXml parseJsonToXml = new ParseJsonToXml(json);
